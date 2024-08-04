590287a0419c6eab0cc88173879859c3054e8d2d
charudatta <152109007c@gmail.com>
2024-08-04 23:31:34 +0530

    refactor 3

--- src/dff/run_dff.ps1
+++
@@ -1,3 +0,0 @@
-iverilog -o test_dff.vvp dff.v test_dff.v
-vvp .\test_dff.vvp
-gtkwave test_dff.vcd
--- src/sequence_counter/run_seq_ctr.ps1
+++
@@ -1,3 +0,0 @@
-iverilog -o test_seq_ctr.vvp seq_ctr.v test_seq_ctr.v
-vvp test_seq_ctr.vvp
-gtkwave test_seq_ctr.vcd
